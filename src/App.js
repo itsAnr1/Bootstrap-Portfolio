49b9fad82d3a62457d521c990d24a106111dfd2d
abhi <abhishekrajun@gmail.com>
2021-01-28 15:34:23 +0530

    Bootstrap portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,157 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <div className="App">React Boilerplate</div>;
+  return (
+    <>
+      <h1 className="text-capatalize text-center mt-5 text-danger">
+        {" "}
+        Welcome to my Page
+      </h1>
+      <div class="container">
+        <div class="row">
+          <div className="col-sm">
+            <br />
+            <br />
+            <div class="card">
+              <img
+                src="https://picsum.photos/201/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <br />
+            <br />
+            <div class="card">
+              <img
+                src="https://picsum.photos/202/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <br />
+            <br />
+            <div class="card">
+              <img
+                src="https://picsum.photos/203/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div className="col-sm">
+            <br />
+            <br />
+            <div class="card">
+              <img
+                src="https://picsum.photos/201/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <br />
+            <br />
+            <div class="card">
+              <img
+                src="https://picsum.photos/202/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <br />
+            <br />
+            <div class="card">
+              <img
+                src="https://picsum.photos/203/300"
+                class="card-img-top"
+                alt="..."
+                height="200px"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
